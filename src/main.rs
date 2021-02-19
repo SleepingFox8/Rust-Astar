3089ab8c5895023caf6f250e1cab3c87942f8d2d
SleepingFox8 <SleepingFox8@gmail.com>
2021-02-19 18:30:12 -0500

    remove unused

--- src/main.rs
+++ src/main.rs
@@ -75,52 +75,19 @@ fn main() {
             return ((distance_between_nodes(node1, node2) / (pathtype_travel_speed(&pathtype_between_nodes(&node1.to_string(), &node2.to_string())).unwrap())) * 1000.0) as u64
         };
 
-        // let get_successors = |node: &str| -> Vec<(&str, f64)>{
-        //     let neighbors = get_neighbors(node);
-
-        //     let mut successors = Vec::new();
-
-        //     for neighbor in neighbors{
-        //         successors.push((&neighbor[..], cost_of_travel_between_nodes(node, neighbor)));
-        //     }
-
-        //     return successors
-        // };
-
-    // DEBUG
-        // println!("distance_between_points: {}", distance_between_points(1,1,1, 1,2,2));
-        // println!("{}",json["0x0000B7E19F1B10894A0B99A4E81DE277"]["connections"].is_object());
-        // print_type_of(&json["0x0000B7E19F1B10894A0B99A4E81DE277"]["connections"]);
-        // println!("Neighbors: {:?}", get_neighbors("0x0000B7E19F1B10894A0B99A4E81DE277"));
-        // println!("pathType: {}", pathtype_for_types(&"test".to_string(), &"normal".to_string()));
-        // println!("pathtype_travel_speed(): {}", pathtype_travel_speed("normal").unwrap());
-        // println!("get_successors: {:?}", get_successors("0x0000B7E19F1B10894A0B99A4E81DE277"));
-
     // declare destination nodeIDs
         let gensokyo_yuyu_hut = "0x0E4B93C652D6C3DF1DF377D1DFA1B33C";
         let gensokyo_cactus_farm = "0x00F936B150C8D7B0363B449FA45710F1";
         let pandoria_station_underground = "0x4C4D4CB26FEBE14BAC7A6E4887BB7375";
 
     // pathfind
-        // let result = astar(&Pos(1, 1), |p| p.neighbours(), |p| p.distance(&GOAL) / 3, |p| *p == GOAL);
         let mut target = pandoria_station_underground;
 
-        let h = |node: &str| -> f64{
-            let distance = distance_between_nodes(&node, &target);
-            return distance / 16.9
-        };
-
-        let success = |node: &str| node == target;
-
-        // let result = astar(&gensokyo_yuyu_hut, get_successors, h, success);
-        // assert_eq!(result.expect("no path found").1, 4);
-
-
-
-
         let result = astar(
+            // starting node
             &gensokyo_yuyu_hut, 
             
+            // get_successors()
             |node| -> Vec<(&str, u64)>{
                 let neighbors = get_neighbors(node);
                 let mut successors = Vec::new();
@@ -129,38 +96,14 @@ fn main() {
                 }
                 return successors
             },
+            // h()
             |node| -> u64{
                 let distance = distance_between_nodes(&node, &target);
                 return (distance / SPRINT_JUMPING_ICE_SPEED * 1000.0) as u64
             },
-            |node| node == &target);
+            // success()
+            |node| node == &target
+        );
 
 
         println!("result: {:?}", result)
-
-
-
-// working example
-    // use pathfinding::prelude::{absdiff};
-
-    // #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-    // struct Pos(i32, i32);
-
-    // impl Pos {
-    //     fn distance(&self, other: &Pos) -> u32 {
-    //         (absdiff(self.0, other.0) + absdiff(self.1, other.1)) as u32
-    //     }
-
-    //     fn successors(&self) -> Vec<(Pos, u32)> {
-    //         let &Pos(x, y) = self;
-    //         vec![Pos(x+1,y+2), Pos(x+1,y-2), Pos(x-1,y+2), Pos(x-1,y-2),
-    //             Pos(x+2,y+1), Pos(x+2,y-1), Pos(x-2,y+1), Pos(x-2,y-1)]
-    //             .into_iter().map(|p| (p, 1)).collect()
-    //     }
-    // }
-
-    // static GOAL: Pos = Pos(4, 6);
-    // let result = astar(&Pos(1, 1), |p| p.successors(), |p| p.distance(&GOAL) / 3, |p| *p == GOAL);
-    // assert_eq!(result.expect("no path found").1, 4);
-    // println!("path works");
-}
