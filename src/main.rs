bb27cb69c9695ef7e2dcadbabcb32e69e11ab645
SleepingFox8 <SleepingFox8@gmail.com>
2021-02-24 21:20:21 -0500

    find path to all destinations on single thread

--- src/main.rs
+++ src/main.rs
@@ -34,11 +34,16 @@
 
 fn main() {
 
-    // retrieve json from file
+    // retrieve node-data json from file
         let mut file = File::open("Public-CivClassic-Nodes/nodes.json").expect("Failed to open file");
         let mut contents = String::new();
         file.read_to_string(&mut contents).expect("Failed to read to string");
-        let json: serde_json::Value = serde_json::from_str(&contents).expect("JSON was not well-formatted");
+        let node_data: serde_json::Value = serde_json::from_str(&contents).expect("JSON was not well-formatted");
+    // retrieve destinations from json
+        let mut file = File::open("Public-CivClassic-Nodes/destinations.json").expect("Failed to open file");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Failed to read to string");
+        let destinations: serde_json::Value = serde_json::from_str(&contents).expect("JSON was not well-formatted");
 
     // declare closures
 
@@ -46,7 +51,7 @@ fn main() {
             let mut neighbors = Vec::new();
 
             // loop through all keys in "connections"
-            for (neighbor, val) in json[node]["connections"].as_object().unwrap(){
+            for (neighbor, val) in node_data[node]["connections"].as_object().unwrap(){
                 neighbors.push(neighbor)
             }
 
@@ -62,11 +67,11 @@ fn main() {
         };
 
         let pathtype_between_nodes = |node1: &str, node2: &str| -> String{
-            pathtype_for_types(&json[node1]["pathType"].as_str().unwrap(), &json[node2]["pathType"].as_str().unwrap())
+            pathtype_for_types(&node_data[node1]["pathType"].as_str().unwrap(), &node_data[node2]["pathType"].as_str().unwrap())
         };
 
         let distance_between_nodes = |node1: &str, node2: &str| -> f64{
-            return distance_between_points(json[node1]["x"].as_i64().unwrap(),json[node1]["y"].as_i64().unwrap(),json[node1]["z"].as_i64().unwrap(), json[node2]["x"].as_i64().unwrap(),json[node2]["y"].as_i64().unwrap(),json[node2]["z"].as_i64().unwrap())
+            return distance_between_points(node_data[node1]["x"].as_i64().unwrap(),node_data[node1]["y"].as_i64().unwrap(),node_data[node1]["z"].as_i64().unwrap(), node_data[node2]["x"].as_i64().unwrap(),node_data[node2]["y"].as_i64().unwrap(),node_data[node2]["z"].as_i64().unwrap())
         };
 
         ///returns cost_of_travel_between_nodes as a "u64" in miliseconds
@@ -93,33 +98,74 @@ fn main() {
             let pandoria_station_surface = "0x101F51B31E1276E45FF42E0660198954";
 
     // pathfind
-        let starting_node = bibelbonse;
-        let mut target = pandoria_station_underground;
+        // let starting_node = bibelbonse;
+        // let mut target = pandoria_station_underground;
 
-        let h = |node: &str| -> u64{
-            let distance = distance_between_nodes(&node, &target);
-            return (distance / SPRINT_JUMPING_ICE_SPEED * 1000.0) as u64
-        };
+        // let h = |node: &str| -> u64{
+        //     let distance = distance_between_nodes(&node, &target);
+        //     return (distance / SPRINT_JUMPING_ICE_SPEED * 1000.0) as u64
+        // };
 
+        // let start_time = SystemTime::now();
+        // let result = astar(
+        //     // starting node
+        //         &starting_node, 
+        //     // get_successors()
+        //         |node|get_successors(node),
+        //     // h()
+        //         |node|h(node),
+        //     // success()
+        //         |node| node == &target
+        // );
+
+        // let end_time = SystemTime::now();
+        // let duration = end_time
+        // .duration_since(start_time)
+        // .expect("Time went backwards");
+
+        // println!("result: {:?}", result);
+        // println!("{:?}", duration);
+
+    // pathfind to all destinations
         let start_time = SystemTime::now();
-        let result = astar(
-            // starting node
-                &starting_node, 
-            // get_successors()
-                |node|get_successors(node),
-            // h()
-                |node|h(node),
-            // success()
-                |node| node == &target
-        );
+        for (node_id, dest_name) in destinations.as_object().unwrap(){
+
+            // pathfind to destination and record time
+                let starting_node = gensokyo_yuyu_hut;
+                let mut target = node_id;
+        
+                let h = |node: &str| -> u64{
+                    let distance = distance_between_nodes(&node, &target);
+                    return (distance / SPRINT_JUMPING_ICE_SPEED * 1000.0) as u64
+                };
+        
+                let result = astar(
+                    // starting node
+                        &starting_node, 
+                    // get_successors()
+                        |node|get_successors(node),
+                    // h()
+                        |node|h(node),
+                    // success()
+                        |node| node == &target
+                );
+        
+                // println!("result: {:?}", result);
+                
+                println!("Destination: {}",dest_name);
+                let eta = 0;
+                match result {
+                    Some(x) => println!("ETA: {}",x.1 / 1000 / 60),
+                    None => println!("ETA: [no path found]"),
+                };
+                
+        }
 
         let end_time = SystemTime::now();
         let duration = end_time
-        .duration_since(start_time)
-        .expect("Time went backwards");
-        
-    println!("result: {:?}", result);
+            .duration_since(start_time)
+            .expect("Time went backwards");
 
-    println!("{:?}", duration);
+        println!("{:?}", duration);
 
 }
